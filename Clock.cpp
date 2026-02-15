3f8c10c868bcc644eedfdd6f22c577afdf1c8ab2
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-02-15 23:00:13 +0530

    The Initial Variable has been created

--- Clock.cpp
+++ Clock.cpp
@@ -1,5 +1,22 @@
-#include<iostream>
+#include <iostream>
+#include <windows.h>
+
 using namespace std;
-int main(){
-    cout<<"Hello world ";
+int main()
+{
+
+    int h, m, s, a, err;
+    err = a = 0;
+    while (err == 0)
+    {
+        cout << "Enter hour  : ";
+        cin>>h;
+        cout<<"Enter minutes : ";
+        cin>>m;
+        cout<<"Enter Seconds : ";
+        cin>>s;
+
+        cout<<"The hour"<<h<<"the minutes"<<m<<"th second"<<s<<endl;
+
+    }
 }
